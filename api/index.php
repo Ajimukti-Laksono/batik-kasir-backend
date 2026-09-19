14c70c9c7c068e44574d402be9bf6d7f98cfd924
Ajimukti-Laksono <202310715033@mhs.ubharajaya.ac.id>
2026-09-19 10:00:56 +0700

    fix: override config cache path to force dynamic resolution

--- api/index.php
+++ api/index.php
@@ -23,11 +23,17 @@ foreach ($directories as $directory) {
     }
 }
 
-// Override Cache Paths
+// Override Cache Paths to force dynamic config resolution on Vercel
 $_ENV['APP_PACKAGES_CACHE'] = '/tmp/storage/bootstrap/cache/packages.php';
 $_ENV['APP_SERVICES_CACHE'] = '/tmp/storage/bootstrap/cache/services.php';
+$_ENV['APP_CONFIG_CACHE'] = '/tmp/storage/bootstrap/cache/config.php';
+$_ENV['APP_ROUTES_CACHE'] = '/tmp/storage/bootstrap/cache/routes.php';
+$_ENV['APP_EVENTS_CACHE'] = '/tmp/storage/bootstrap/cache/events.php';
 putenv('APP_PACKAGES_CACHE=' . $_ENV['APP_PACKAGES_CACHE']);
 putenv('APP_SERVICES_CACHE=' . $_ENV['APP_SERVICES_CACHE']);
+putenv('APP_CONFIG_CACHE=' . $_ENV['APP_CONFIG_CACHE']);
+putenv('APP_ROUTES_CACHE=' . $_ENV['APP_ROUTES_CACHE']);
+putenv('APP_EVENTS_CACHE=' . $_ENV['APP_EVENTS_CACHE']);
 
 // Fix empty driver issues from Vercel UI overriding with empty strings
 $defaults = [
